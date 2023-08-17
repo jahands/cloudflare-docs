e53464cad699282dd2a97065d3946d1207b5ef1b
Jacob Hands <jacob@gogit.io>
2023-08-17 14:08:47 -0500

    Try rebuild

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 cd ./developers.cloudflare.com 
 echo "Installing dependencies..."
 time yarn install --pure-lockfile
+echo "Rebuilding packages..."
+time yarn rebuild
 echo "Building..."
 time yarn build:incremental
