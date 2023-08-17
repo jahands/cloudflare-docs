7fe8e05ccaa4b415abee227cab82f0e8f8d840c0
Jacob Hands <jacob@gogit.io>
2023-08-17 13:50:30 -0500

    Don't clone

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-git clone https://github.com/cloudflare/cloudflare-docs.git
-cd ./cloudflare-docs
-git checkout 42a971eb44387e40ea9a9907c519757d0518d950
 cd ./developers.cloudflare.com 
 echo "Installing dependencies..."
 time yarn install --pure-lockfile
